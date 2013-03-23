0509ffc18f1bd31535fc61aa1c8852442863c522
hamurabi69 <jham@infolink.com>
2013-03-23 15:31:52 -0400

    renamed:    admin/choose_page.php -> admin/select_catagory.php 	modified:   admin/choose_catagory.php

--- admin/select_catagory.php
+++ admin/select_catagory.php
@@ -8,40 +8,24 @@ error_reporting(-1);
 include('../includes/db.php');
         require('header.php');
 
-//added php-mysql security
-        $dept_id = mysqli_real_escape_string($db, strip_tags($_GET['id']));
 
         if($_POST['edit']){
 //Added sql security to prevent sql injection
                 $name = mysqli_real_escape_string($db, strip_tags( $_POST['name']));
                 $id = mysqli_real_escape_string($db, strip_tags( $_POST['id']));
 //Refer to correct page for edit
-                        header('Location: edit_article.php?id='.$id);
+                        header('Location: choose_page.php?id='.$id);
                         exit();
                 }    
-
-        if($_POST['activate']){
+        if($_POST['delete']){
 //Added sql security to prevent sql injection
-                $id = mysqli_real_escape_string($db, strip_tags( $_POST['id']));
-                $name = mysqli_real_escape_string($db, strip_tags( $_POST['name']));
-//Set status to 1 if activating
-                mysqli_query($db, "UPDATE tbl_pages SET status='1' WHERE id='$id'");
-                mysqli_close($db);
-                        header('Location: edit_page.php?id='.$dept_id);
-                        exit();
-                }    
-
-        if($_POST['deactivate']){
-//Added sql security to prevent sql injection
-                $id = mysqli_real_escape_string($db, strip_tags( $_POST['id']));
                 $name = mysqli_real_escape_string($db, strip_tags( $_POST['name']));
 //Set status to 0 if deactivating
-                mysqli_query($db, "UPDATE tbl_pages SET status='0' WHERE id='$id'");
+                mysqli_query($db, "UPDATE tbl_dept SET status='0' WHERE name='$name'");
                 mysqli_close($db);
-                        header('Location: edit_page.php?id='.$dept_id);
+                        header('Location: index.php');
                         exit();
                 }    
-
 //return to home page
         if($_POST['exit']){
                         header('Location: index.php');
@@ -61,48 +45,26 @@ include('../includes/db.php');
 
         <table border="1" class="table1">
                 <tr>
-                        <th><h2>Edit a Web Page</h2></th>
+                        <th><h2>Choose a Catagory</h2></th>
                 </tr>
                 <tr>
                 <td>
                 <table>
-				<tr>
-				<th>Page Name</th>
-				<th>Status</th>
-				<th>Sort Order</th>
-				</tr>
+
 <?php
 //Retrieve required information from DB and display on page
-			$tresults = mysqli_query($db, "SELECT * FROM tbl_pages WHERE dept_id='$dept_id' ORDER BY p_sort");
+			$tresults = mysqli_query($db, "SELECT * FROM tbl_dept WHERE status=1 ORDER BY sort_order");
                                         if( $trow = mysqli_fetch_array($tresults)){
                                                 do{
-						$name=$trow['p_title'];
-						$status=$trow['status'];
-						$p_sort=$trow['p_sort'];
+						$name=$trow['name'];
 						$id=$trow['id'];
 ?>
 				<form name="edit" method="post" action="<?php basename($PHP_SELF)?>">
                                 <tr>
 				<td><?php echo $name ?></td>
-				<td>
-				<?php 
-					switch($status){
-					case "0":
-						$status="Inactive";
-						break;
-					case "1":
-						$status="Active";
-						break;
-					default:
-						$status="Unknown";
-				}
-				echo $status ?>
-				</td>
-				<td><?php echo $p_sort ?></td>
-				<td><input type="hidden" name="id" value="<?php echo $id ?>">
-				<input type="submit" name="edit" value="Edit" class="button"/></td>
-				<td><input type="submit" name="deactivate" value="Deactivate" class="button"/></td>
-				<td><input type="submit" name="activate" value="Activate" class="button"/></td>
+				<td><input type="hidden" name="id" value="<?php echo $id ?>"></td>
+				<td><input type="submit" name="edit" value="Edit" class="button"/></td>
+				<td><input type="submit" name="delete" value="Deactivate" class="button"/></td>
                                 </tr>
 				</form>
 <?php
