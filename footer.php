c17bcf233446270e4033e1c5b6aa3e5304f2bc93
hamurabi69 <jham@infolink.com>
2013-03-27 19:52:26 -0400

    new file:   footer.php

---
+++ footer.php
@@ -0,0 +1,22 @@
+<ul id="figo">
+  <a href="#"><span>Admin</span></a>
+		<a href="#"><span>Living Toolkit</span></a>
+		<a href="#"><span>Downloads</span></a>
+		<a href="#"><span>Links</span></a>
+		<a href="#"><span>Contact us</span></a>
+		<a href="#"><span>Home</span></a>
+</ul>
+
+<marquee style="background-color: #eee; border: 1px solid #ccc">
+<p>For more information go to www.211-broward.org/</p>
+</marquee>
+
+</div>
+<hr width="100%">
+ <div class="footer"> Copyright 2013 Carlos, Jon, Zickster | Capstone Project | Broward College
+ 
+ </div>   
+ </body> 
+ </html> 
+
+
